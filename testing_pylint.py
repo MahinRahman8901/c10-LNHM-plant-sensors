df751a5f52b8b1fd42ff567d3d2e7eaeae61a0ac
Isaac Schaessens Coleman <trainee.isaac.schaessens.coleman@sigmalabs.co.uk>
2024-04-15 12:32:20 +0100

    testing pylint worflow #2

--- testing_pylint.py
+++ testing_pylint.py
@@ -1,6 +1,8 @@
+'''This script is simply used to test the pylint workflow in the github actions'''
 
+GLOBAL_VARIABLE = 100
 
-print('hello world')
+if __name__ == "__main__":
 
-# variable that isn't used
-a = 100
+    print("Hello World")
+    print(GLOBAL_VARIABLE)
