f93f77e8a24a5b378bfe5e7cbdcf682abc2a4d0e
Isaac Schaessens Coleman <trainee.isaac.schaessens.coleman@sigmalabs.co.uk>
2024-04-16 15:45:13 +0100

    Added BotanistPlantRecordTable and changed some attributes

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Botanist(
+CREATE TABLE s_epsilon.Botanist(
     BotanistID INT IDENTITY(1,1) PRIMARY KEY,
     FirstName varchar(20) NOT NULL, 
     LastName varchar(30) NOT NULL,
@@ -6,23 +6,24 @@ CREATE TABLE Botanist(
     Phone varchar(25)
     )
 
-CREATE TABLE Location(
+CREATE TABLE s_epsilon.Location(
     LocationID INT IDENTITY(1,1) PRIMARY KEY,
+    Longitude DECIMAL(11,6) NOT NULL,
+    Latitude DECIMAL (11,6) NOT NULL,
     Town varchar(50) NOT NULL,
     City varchar(50) NOT NULL,
     CountryCode varchar(50) NOT NULL UNIQUE,
     Continent varchar(15) NOT NULL
 )
 
-CREATE TABLE Plant(
+CREATE TABLE s_epsilon.Plant(
     PlantID INT IDENTITY(1,1) PRIMARY KEY,
     Name varchar(50) NOT NULL,
     ScientificName varchar(50),
     LocationID INT FOREIGN KEY REFERENCES Location(LocationID) NOT NULL,
-    BotanistID INT FOREIGN KEY REFERENCES Botanist(BotanistID) NOT NULL
 )
 
-CREATE TABLE PlantMeasurementRecord(
+CREATE TABLE s_epsilon.PlantMeasurementRecord(
     MeasurementRecordID INT IDENTITY(1,1) PRIMARY KEY,
     TimeRecorded SMALLDATETIME NOT NULL,
     SoilMoisture DECIMAL(5,2) NOT NULL,
@@ -30,24 +31,14 @@ CREATE TABLE PlantMeasurementRecord(
     PlantID INT FOREIGN KEY REFERENCES Plant(PlantID)
 )
 
-CREATE TABLE WateredRecord(
+CREATE TABLE s_epsilon.WateredRecord(
     WateredRecordID INT IDENTITY(1,1) PRIMARY KEY,
     TimeWatered SMALLDATETIME NOT NULL,
     PlantID INT FOREIGN KEY REFERENCES Plant(PlantID)
 )
 
-
-INSERT INTO Botanist(FirstName, LastName, Email, Phone)
-VALUES ('Isaac', 'Coleman', 'hopefullyidontmessthingsup@gmail.com', '46466')
-
-INSERT INTO Location(Town, City, CountryCode, Continent)
-VALUES ('South Whittier', 'LosAngeles', 'US', 'America')
-
-INSERT INTO Plant(Name, ScientificName, LocationID, BotanistID)
-VALUES ('Venus flytrap', 'NULL',1, 1)
-
-INSERT INTO PlantMeasurementRecord(TimeRecorded, SoilMoisture, Temperature, PlantID)
-VALUES ('2024-03-14 12:30:45', 30.25, 11.34, 1)
-
-INSERT INTO WateredRecord(TimeWatered, PlantID)
-VALUES ('2024-03-14 12:30:45', 1)
+CREATE TABLE s_epsilon.BotanistPlantRecord(
+    TimeRecorded SMALLDATETIME NOT NULL,
+    PlantID INT FOREIGN KEY REFERENCES Plant(PlantID),
+    BotanistID INT FOREIGN KEY REFERENCES Botanist(BotanistID)
+)
