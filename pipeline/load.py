f85bf15e63c2639067dd97aaa5bd084a0c2cbfe7
Isaac Schaessens Coleman <trainee.isaac.schaessens.coleman@sigmalabs.co.uk>
2024-04-17 14:14:50 +0100

    Fixed load script functionality

--- pipeline/load.py
+++ pipeline/load.py
@@ -1,10 +1,10 @@
-'''A load script as part of the data pipeline. This script moves the data from the csv file, produced by the
-transform script, to a database.'''
+'''A load script as part of the data pipeline. This script moves the data from the csv file,
+produced by the transform script, to a database.'''
 
 from csv import DictReader
 from datetime import datetime
-from dotenv import load_dotenv
 from os import environ as ENV
+from dotenv import load_dotenv
 from pymssql import connect
 
 
@@ -19,7 +19,6 @@ def get_database_connection(config):
         port=int(config["DB_PORT"]),
     )
 
-
 def get_botanist_id_dictionary(conn) -> dict:
     """Given a database connection (designed for our specific plants database), this
     function creates a dictionary with the keys being the botanist's emails and the value's
@@ -31,7 +30,6 @@ def get_botanist_id_dictionary(conn) -> dict:
         cursor.execute('SELECT * FROM s_epsilon.Botanist')
         for row in cursor:
             botanist_dict[row['Email']] = row['BotanistID']
- 
     return botanist_dict
 
 
@@ -39,8 +37,8 @@ def read_csv(csv_file_path:str) -> list[dict[str]]:
     '''This function reads a csv file and outputs a list of dictionaries - 
      Each dictionary corresponds to a row in the csv file where the keys are
      the elements in the first row of the file.
-     Example: {'gertrude.jekyll@lnhm.co.uk': 1, 'carl.linnaeus@lnhm.co.uk': 2, 'eliza.andrews@lnhm.co.uk': 3}'''
-    
+     Example: {'gertrude.jekyll@lnhm.co.uk': 1, 'carl.linnaeus@lnhm.co.uk': 2,
+       'eliza.andrews@lnhm.co.uk': 3}'''
     with open(csv_file_path, 'r') as f:
         data = DictReader(f)
         data_list = list(data)
@@ -60,22 +58,24 @@ def db_query_string(data:list[dict], conn) -> str:
 
     output_string = ""
     for row in data:
-        # TimeRecorded, SoilMoisture, Temperature, TimeLastWatered, PlantID, BotanistID    
-        output_string += f"( '{row['recording_taken']}', {float(row['soil_moisture'])}, {float(row['temperature'])}, '{str(datetime.strptime(row['last_watered'], '%a, %d %b %Y %H:%M:%S %Z'))}', {int(row['id'])}, {int(botanist_dict[row['botanist_email']])} ),"
+        # TimeRecorded, SoilMoisture, Temperature, TimeLastWatered, PlantID, BotanistID
+        output_string += f"""( '{row['recording_taken']}', {float(row['soil_moisture'])},
+          {float(row['temperature'])}, '{str(datetime.strptime(row['last_watered'], '%a, %d %b %Y %H:%M:%S %Z'))}',
+            {int(row['id'])}, {int(botanist_dict[row['botanist_email']])} ),"""
 
 
     # Removing final unnecessary comma
-    return output_string[:-2]
+    return output_string[:-1]
 
-   
-def db_inserting_data(query_string:str, conn):
+
+def db_inserting_data(query_string:str, conn) -> None:
     """This function inserts data into the PlantMeasurementRecord table, using the 
     inputted query string for the data."""
 
     with conn.cursor(as_dict=True) as cursor:
-        cursor.execute("""INSERT INTO s_epsilon.PlantMeasurementRecord (TimeRecorded, SoilMoisture, Temperature, TimeLastWatered, PlantID, BotanistID)
-                       VALUES %s""", query_string)
- 
+        cursor.execute(f"""INSERT INTO s_epsilon.PlantMeasurementRecord (TimeRecorded, SoilMoisture,
+                        Temperature, TimeLastWatered, PlantID, BotanistID)
+                       VALUES {query_string}""")
         conn.commit()
 
 if __name__ == "__main__":
@@ -83,22 +83,19 @@ if __name__ == "__main__":
     load_dotenv()
 
     print('before connection')
+    # connects to database
     conn = get_database_connection(ENV)
     print('after connection')
-
-
-
-    botanist_dict = get_botanist_id_dictionary(conn)
-    print(f"Botanist Dict: {botanist_dict}")
-
-    
+ 
+    # data from csv as list of dictionaries
     data = read_csv("plants_data.csv")
-    query_string = "('2024-04-17 08:10:20', 35.29, 12.02, '2024-04-16 13:54:32', 1, 1 )"
-    print(query_string)
+ 
+    # the values section of the insert query string
+    query_string = db_query_string(data, conn)
+
     print('before inserting into db')
     db_inserting_data(query_string, conn)
     print('after inserting into db')
-
 
 
     
