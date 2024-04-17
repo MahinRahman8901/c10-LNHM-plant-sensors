d6360a1892137d9d9027988754c7ab7ca0a1dc3a
Isaac Schaessens Coleman <trainee.isaac.schaessens.coleman@sigmalabs.co.uk>
2024-04-17 10:47:49 +0100

    Added load script and 'PyMySQL' to requirements

---
+++ pipeline/load.py
@@ -0,0 +1,7 @@
+'''A load script as part of the data pipeline. This script moves the data from the csv file, produced by the
+transform script, to a database.'''
+
+
+if __name__ == "__main__":
+
+    pass
