bde88e75e0a7fdaa1347824ba0ce8fa55592f04f
MahinRahman <trainee.mahin.rahman@sigmalabs.co.uk>
2024-04-17 12:22:38 +0100

    updates

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -7,6 +7,7 @@ import asyncio
 
 API_URL = 'https://data-eng-plants-api.herokuapp.com/plants/'
 
+
 async def extract_plant_data() -> list[dict]:
     """Scrapes information from API asynchronously and returns a list of dictionaries"""
 
@@ -56,42 +57,6 @@ async def extract_data_for_each_plant(session, plant_id) -> dict:
 
 def create_csv_file(data: list[dict], filename: str):
     """Creates CSV file where all data collated from the API is stored"""
-    response = requests.get(url)
-    if response.status_code == 200:
-        plant_info = response.json()
-        data_to_append = {
-            'id': plant_info.get('plant_id'),
-            'name': plant_info.get('name'),
-            'soil_moisture': plant_info.get('soil_moisture'),
-            'temperature': plant_info.get('temperature'),
-            'last_watered': plant_info.get('last_watered'),
-            'recording_taken': plant_info.get('recording_taken'),
-            'botanist_name': plant_info['botanist']['name'],
-            'botanist_email': plant_info['botanist']['email'],
-            'botanist_phone': plant_info['botanist']['phone']
-        }
-
-        if 'light_intensity' in plant_info:
-            data_to_append['light_intensity'] = plant_info['light_intensity']
-
-        if 'humidity' in plant_info:
-            data_to_append['humidity'] = plant_info['humidity']
-
-        if 'origin_location' in plant_info:
-            origin_location = plant_info['origin_location']
-            if len(origin_location) >= 3:
-                data_to_append['latitude'] = origin_location[0]
-                data_to_append['longitude'] = origin_location[1]
-                data_to_append['origin_location'] = origin_location[-3:]
-
-        plant_data.append(data_to_append)
-    else:
-        print(f"Could not find plant {plant_id}")
-
-
-def create_csv_file(data: str, filename: str):
-    """Creates csv file where all data collated
-    from the api is stored and can be viewed"""
     keys = data[0].keys() if data else []
     with open(filename, 'w', newline='', encoding='utf-8') as f:
         writer = csv.DictWriter(f, fieldnames=keys)
